41ae7f695dd491a980c7a4c70410a455d1ae0c80
cdmackinnon <89928233+cdmackinnon@users.noreply.github.com>
2025-04-05 11:06:27 -0400

    Scrape School Names
    Scraped 8000 urls for school names. Stored them in a json with {id:name} pairings.

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from src.scraping import ProfessorScraper
 import json
 
 def main():
+    get_university_names()
+
+
+def get_university_names():
     NUM_UNIVERSITIES = 8000
     school_data = {}
+    test = ProfessorScraper()
 
     for id in range(1, NUM_UNIVERSITIES):
         name = test.fetch_school_name(id)
@@ -12,20 +17,9 @@ def main():
             school_data[id] = name
 
     # Save dictionary to JSON file
-    with open("data/school_data.json", "w") as f:
+    with open("data/school_names.json", "w") as f:
         json.dump(school_data, f, indent=4)
 
-
-    # url = "https://www.ratemyprofessors.com/search/professors/{insert_school_id}?q="
-    # Test scraping
-    test = ProfessorScraper()
-    id  = 5000
-    school_name = test.fetch_school_name(id)
-    test.read_page_source(f"https://www.ratemyprofessors.com/search/professors/{id}?q=*", school_name)
-    test.quit()
-
-    # Test parsing
-    test.parse_professors(school_name)
 
 if __name__ == "__main__":
     main()
