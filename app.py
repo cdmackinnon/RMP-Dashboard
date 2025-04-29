4a1bb7f46b665de6576aba5d72868fece88be429
cdmackinnon <89928233+cdmackinnon@users.noreply.github.com>
2025-04-29 17:54:59 -0400

    Add sleek UI for department graphs

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.graph_objs as go
 import plotly.io as pio
 import plotly.utils
 import json
+from flask import request, jsonify
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///rmp.db"
@@ -20,9 +21,6 @@ def initialize_database(app):
             with open("db/schema.sql", "r") as f:
                 db.session.execute(text(f.read()))
             db.session.commit()
-
-
-from flask import request, jsonify
 
 
 @app.route("/autocomplete")
@@ -41,7 +39,12 @@ def autocomplete():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return "Index"
+
+
+@app.route("/departments")
+def school():
+    return render_template("departments.html")
 
 
 @app.route("/school_plot")
@@ -90,72 +93,22 @@ def school_plot():
         x=departments,
         y=avg_difficulties,
         text=[f"{tr} ratings" for tr in total_ratings],
-        marker_color="indianred",
+        marker_color="#FF9149",
     )
 
     layout = go.Layout(
-        title=f"Average Difficulty per Department at {school_name}",
+        title=f"Department Difficulties at {school_name}",
         xaxis=dict(title="Department", tickangle=45),
-        yaxis=dict(title="Avg Difficulty"),
+        yaxis=dict(title="Average Professor Difficulty"),
         height=600,
         margin=dict(b=150),
+        paper_bgcolor="#FFEDDB",
     )
 
     fig = go.Figure(data=[bar], layout=layout)
     graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return jsonify({"graphJSON": graph_json})
-
-
-# @app.route("/")
-# def index():
-#     with db.engine.connect() as connection:
-#         result = connection.execute(
-#             text(
-#                 """
-#             SELECT
-#                 s.school_name,
-#                 s.school_id,
-#                 ROUND(SUM(i.quality * i.total_ratings) / SUM(i.total_ratings), 2) AS avg_quality,
-#                 ROUND(SUM(i.difficulty * i.total_ratings) / SUM(i.total_ratings), 2) AS avg_difficulty
-#             FROM
-#                 Schools s
-#             JOIN
-#                 Instructors i ON s.school_id = i.school_id
-#             GROUP BY
-#                 s.school_id, s.school_name
-#             HAVING
-#                 SUM(i.total_ratings) > 100;
-
-#         """
-#             )
-#         )
-
-#         data = result.fetchall()
-#     school_names = [row[0] for row in data]
-#     avg_qualities = [row[2] for row in data]
-#     avg_difficulties = [row[3] for row in data]
-
-#     scatter = go.Scatter(
-#         x=avg_difficulties,
-#         y=avg_qualities,
-#         mode="markers+text",
-#         hovertext=school_names,
-#         # text= school_names,
-#         textposition="top center",
-#         marker=dict(size=12, color="skyblue"),
-#     )
-
-#     layout = go.Layout(
-#         title="Average Quality vs Difficulty per School",
-#         xaxis=dict(title="Average Difficulty"),
-#         yaxis=dict(title="Average Quality"),
-#         height=600,
-#     )
-
-#     fig = go.Figure(data=[scatter], layout=layout)
-#     graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-#     return render_template("index.html", graphJSON=graph_json)
 
 
 if __name__ == "__main__":
